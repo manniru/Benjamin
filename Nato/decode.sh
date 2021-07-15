3ce478a11e79f50994858fbff52c255a0fcbdb28
bijanbina <bijanbina@gmail.com>
2021-07-15 11:25:53 +0430

    Migrate from Kaldi to Benjamin. Now all development are doing in place

--- Nato/decode.sh
+++ Nato/decode.sh
@@ -29,10 +29,11 @@ SPK2UTT="$AUDIO_PATH/spk2utt"
 CMVN="$AUDIO_PATH/cmvn.scp"
 FEAT="$AUDIO_PATH/feats.scp"
 
-LATTICE="$RESULT_PATH/lattice.ark"
+LATTICE_FILE="ark,t:$RESULT_PATH/lattice.ark"
 RESULT="$RESULT_PATH/best.tra"
 
 LATGEN_OPTIONS="--max-active=7000 --beam=13.0 --lattice-beam=6.0 --acoustic-scale=0.083333 --allow-partial=true --word-symbol-table=$WORD_TABLE $FINAL_MDL $HCLG_FST"
+LATGEN_CMD="ark,s,cs:apply-cmvn  --utt2spk=ark:$UTT2SPK scp:$CMVN scp:$FEAT ark:- | add-deltas  ark:- ark:- |"
 
 MFCC_DIR="$DECODE_PATH/mfcc"
 LOG_DIR="$DECODE_PATH/log"
@@ -41,8 +42,7 @@ utils/utt2spk_to_spk2utt.pl $UTT2SPK > $SPK2UTT
 steps/make_mfcc.sh --nj 1 $AUDIO_PATH $LOG_DIR $MFCC_DIR > /dev/null
 steps/compute_cmvn_stats.sh $AUDIO_PATH $LOG_DIR $MFCC_DIR > /dev/null
 
-gmm-latgen-faster $LATGEN_OPTIONS "ark,s,cs:apply-cmvn  --utt2spk=ark:$UTT2SPK scp:$CMVN scp:$FEAT ark:- | add-deltas  ark:- ark:- |" ark,t:$LATTICE  2>&1 | grep ^bijan
+BUF=$(gmm-latgen-faster $LATGEN_OPTIONS "$LATGEN_CMD" $LATTICE_FILE 2>&1 | grep ^bijan)
+echo $BUF
 
-
-
-	
+dbus-send --session --dest=com.binaee.rebound  / com.binaee.rebound.speex string:"$BUF"
