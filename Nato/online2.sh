b7dd41f4504cbcf35b21d46054d5c22f03a11638
bijanbina <bijanbina@gmail.com>
2021-09-14 17:59:03 +0430

    NATO: Add up all config files

--- Nato/online2.sh
+++ Nato/online2.sh
@@ -11,5 +11,5 @@ WAV_SCP="online/wav.scp"
 SPK2UTT="online/spk2utt"
 FINAL_MDL="exp/$MODE/final.mdl"
 
-LATGEN_OPTIONS="--config=exp/tri3_online/conf/online_decoding.conf --max-active=7000 --beam=40.0 --lattice-beam=6.0 --acoustic-scale=0.05 --word-symbol-table=$WORD_TABLE --model=$FINAL_MDL"
+LATGEN_OPTIONS="--config=exp/tri3_online/conf/online_decoding.conf --max-active=7000 --beam=12.0 --lattice-beam=6.0 --acoustic-scale=0.05 --word-symbol-table=$WORD_TABLE --model=$FINAL_MDL"
 online2-wav-gmm-latgen-faster $LATGEN_OPTIONS $HCLG_FST ark:$SPK2UTT scp:$WAV_SCP $OUT_LAT
