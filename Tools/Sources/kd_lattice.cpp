88dad390fff2ef64b3cf69c7dbdf98ddc3ca499b
bijanbina <bijanbina@gmail.com>
2021-12-11 16:01:45 +0330

    BaTool: Lets Drop IntelMKL Dependency soon! ShortestPath FST is Here

--- Tools/Sources/kd_lattice.cpp
+++ Tools/Sources/kd_lattice.cpp
@@ -2,22 +2,101 @@
 
 using namespace kaldi;
 
-void kd_fstShortestPath(Lattice *ifst, Lattice *ofst)
+bool kd_SingleShortestPath(Lattice *ifst, std::vector<LatticeArc::Weight> *distance,
+        KdStateId *f_parent, std::vector<std::pair<KdStateId, size_t>> *parent)
 {
-    int nshortest = 1;
-    bool unique = false;
-    bool first_path = false;
-    LatticeArc::Weight weight_threshold = LatticeArc::Weight::Zero();
-    KdStateId state_threshold = KD_INVALID_STATE;
-    float delta = KD_SHORTEST_DELTA;
-    std::vector<LatticeArc::Weight> distance;
+    parent->clear();
     fst::AnyArcFilter<LatticeArc> arc_filter;
-    fst::AutoQueue<KdStateId> state_queue(*ifst, &distance, arc_filter);
-    fst::ShortestPathOptions<LatticeArc, fst::AutoQueue<KdStateId>, fst::AnyArcFilter<LatticeArc>> opts(
-                &state_queue, arc_filter, nshortest, unique, false, delta, first_path,
-                weight_threshold, state_threshold);
-    fst::ShortestPath(*ifst, ofst, &distance, opts);
+    fst::AutoQueue<KdStateId> state_queue(*ifst, distance, arc_filter);
+    *f_parent = KD_INVALID_STATE;
+
+    if( ifst->Start()==KD_INVALID_STATE )
+        return true;
+    std::vector<bool> enqueued;
+    KdStateId source = ifst->Start();
+    bool final_seen = false;
+    LatticeArc::Weight f_distance = LatticeArc::Weight::Zero();
+    distance->clear();
+    state_queue.Clear();
+    while (distance->size() < source)
+    {
+        distance->push_back(LatticeArc::Weight::Zero());
+        enqueued.push_back(false);
+        parent->emplace_back(KD_INVALID_STATE, KD_INVALID_ARC);
+    }
+    distance->push_back(LatticeArc::Weight::One());
+    parent->emplace_back(KD_INVALID_STATE, KD_INVALID_ARC);
+    state_queue.Enqueue(source);
+    enqueued.push_back(true);
+    while (!state_queue.Empty())
+    {
+        const auto s = state_queue.Head();
+        state_queue.Dequeue();
+        enqueued[s] = false;
+        const auto sd = (*distance)[s];
+        // If we are using a shortest queue, no other path is going to be shorter
+        // than f_distance at this point.
+
+        if (ifst->Final(s) != LatticeArc::Weight::Zero())
+        {
+            const auto plus = Plus(f_distance, Times(sd, ifst->Final(s)));
+            if (f_distance != plus)
+            {
+                f_distance = plus;
+                *f_parent = s;
+            }
+            if (!f_distance.Member())
+                return false;
+            final_seen = true;
+        }
+
+        for( fst::ArcIterator<fst::Fst<LatticeArc>> aiter(*ifst, s); !aiter.Done(); aiter.Next() )
+        {
+            const auto &arc = aiter.Value();
+            while (distance->size() <= arc.nextstate)
+            {
+                distance->push_back(LatticeArc::Weight::Zero());
+                enqueued.push_back(false);
+                parent->emplace_back(KD_INVALID_STATE, KD_INVALID_ARC);
+            }
+
+            auto &nd = (*distance)[arc.nextstate];
+            const auto weight = Times(sd, arc.weight);
+
+            if( nd!=Plus(nd, weight) )
+            {
+                nd = Plus(nd, weight);
+                if (!nd.Member())
+                    return false;
+                (*parent)[arc.nextstate] = std::make_pair(s, aiter.Position());
+                if (!enqueued[arc.nextstate])
+                {
+                    state_queue.Enqueue(arc.nextstate);
+                    enqueued[arc.nextstate] = true;
+                }
+                else
+                {
+                    state_queue.Update(arc.nextstate);
+                }
+            }
+        }
+    }
+    return true;
 }
 
-// FinalRelativeCost()!=KD_INFINITY --> ReachedFinal
+void kd_fstShortestPath(Lattice *ifst, Lattice *ofst)
+{
+    std::vector<LatticeArc::Weight> distance;
+    std::vector<std::pair<KdStateId, size_t>> parent;
+    KdStateId f_parent;
 
+    if( kd_SingleShortestPath(ifst, &distance, &f_parent, &parent))
+    {
+        fst::internal::SingleShortestPathBacktrace(*ifst, ofst, parent, f_parent);
+    }
+    else
+    {
+        qDebug() << "kd_ShortestPath is fucked";
+    }
+    return;
+}
--- Tools/Sources/kd_lattice.h
+++ Tools/Sources/kd_lattice.h
@@ -1,9 +1,11 @@
 #ifndef KD_LATTICE_H
 #define KD_LATTICE_H
 
+#include <QDebug>
 #include "lat/kaldi-lattice.h"
 
 #define KD_INVALID_STATE  -1  // Not a valid state ID.
+#define KD_INVALID_ARC    -1  // Null Arc
 #define KD_SHORTEST_DELTA 1e-6
 
 typedef fst::StdFst        KdFST;
@@ -11,6 +13,8 @@ typedef fst::StdFst::Arc   KdArc;
 typedef KdArc::StateId     KdStateId;
 
 void kd_fstShortestPath(kaldi::Lattice *ifst, kaldi::Lattice *ofst);
+bool SingleShortestPath(kaldi::Lattice *ifst, std::vector<kaldi::LatticeArc::Weight> *distance,
+        KdStateId *f_parent, std::vector<std::pair<KdStateId, size_t>> *parent);
 
 
 #endif // KD_LATTICE_H
