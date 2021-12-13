292ef9269a3c045fa12750c3d1329c6038a768e3
bijanbina <bijanbina@gmail.com>
2021-12-13 17:46:37 +0330

    BaTool: Maybe Need to Remove ForwardLink

--- Tools/Sources/kd_token2.cpp
+++ Tools/Sources/kd_token2.cpp
@@ -2,10 +2,21 @@
 #include <QDebug>
 
 using namespace kaldi;
-KdToken2::KdToken2(float total_cost, float e_cost, ForwardLinkT *link, KdToken2 *next_tok)
+KdToken2::KdToken2(float total_cost, float e_cost, KdFLink *link, KdToken2 *next_tok)
 {
     tot_cost = total_cost;
     extra_cost = e_cost;
     links = link;
     next = next_tok;
+}
+
+KdFLink::KdFLink(KdToken2 *n_tok, Label in_label, Label out_label,
+                 float g_cost, float a_cost, KdFLink *n_link)
+ {
+      next_tok = n_tok;
+      in_label = ilabel;
+      out_label = olabel;
+      graph_cost = g_cost;
+      acoustic_cost(a_cost);
+      next = n_link;
 }
--- Tools/Sources/kd_token2.h
+++ Tools/Sources/kd_token2.h
@@ -1,22 +1,46 @@
-#ifndef KD_FASTER_TOKEN_H
-#define KD_FASTER_TOKEN_H
+#ifndef KD_TOKEN2_H
+#define KD_TOKEN2_H
 
+#include "lat/kaldi-lattice.h"
 #include "decoder/lattice-faster-decoder.h"
+
+#define KD_INVALID_STATE  -1  // Not a valid state ID.
+#define KD_INVALID_ARC    -1  // Null Arc
+typedef fst::StdFst        KdFST;
+typedef fst::StdFst::Arc   KdArc;
+typedef KdArc::StateId     KdStateId;
 
 class KdToken2
 {
 public:
-  using ForwardLinkT = kaldi::decoder::ForwardLink<KdToken2>;
 
-  // tot_cost is the total (LM + acoustic) cost from the beginning of the
-  // utterance up to this point.
-  float tot_cost;
-  float extra_cost; // always >= 0
+    // tot_cost is the total (LM + acoustic) cost from the beginning of the
+    // utterance up to this point.
+    float tot_cost;
+    float extra_cost; // always >= 0
 
-  ForwardLinkT *links;
-  KdToken2 *next;
+    KdFLink *links;
+    KdToken2 *next;
+    KdStateId state = KD_INVALID_STATE;
 
-  KdToken2(float tot_cost, float extra_cost, ForwardLinkT *links, KdToken2 *next);
+    KdToken2(float tot_cost, float extra_cost, KdFLink *links, KdToken2 *next);
 };
 
-#endif // KD_FASTER_TOKEN_H
+class KdFLink
+{
+public:
+    using Label = fst::StdArc::Label;
+
+    KdToken2 *next_tok;  // the next token [or NULL if represents final-state]
+    int ilabel;  // ilabel on arc
+    int olabel;  // olabel on arc
+    float graph_cost;  // graph cost of traversing arc (contains LM, etc.)
+    float acoustic_cost;  // acoustic cost (pre-scaled) of traversing arc
+    KdFLink *next;  // next in singly-linked list of forward arcs (arcs
+    // in the state-level lattice) from a token.
+    KdFLink(KdToken2 *next_tok, int ilabel, int olabel,
+            float graph_cost, float acoustic_cost,
+            KdFLink *next);
+};
+
+#endif // KD_TOKEN2_H
