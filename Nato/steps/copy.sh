4d951e53bb7ef0527ebd79f1c765a11e0f49d456
bijanbina <bijanbina@gmail.com>
2022-04-15 18:31:46 +0200

    Nato: Make Nato Compatible with Arbade

--- Nato/steps/copy.sh
+++ Nato/steps/copy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+if [[ ! -e "../Tools/Model/" ]]; then
+	mkdir "../Tools/Model/"
+fi
 cp "exp/tri1/graph/HCLG.fst" "../Tools/Model/HCLG.fst"
 cp "exp/tri1/graph/words.txt" "../Tools/Model/words.txt"
 cp "exp/tri1_online/final.oalimdl" "../Tools/Model/final.oalimdl"
