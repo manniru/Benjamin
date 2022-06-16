324c36b2a12a51d7797743f8b404d18597e7895b
bijanbina <bijanbina@gmail.com>
2022-06-16 18:02:33 +0430

    ENN: Add AVX Instruction, Replace ENN Network

--- ENN/ENN.pro
+++ ENN/ENN.pro
@@ -1,17 +1,22 @@
 TEMPLATE = app
 
-QT += multimedia
-
 CONFIG += console
 
 linux:INCLUDEPATH += ./Sources
 
 #linux:LIBS += -lgio-2.0
 
-DEFINES += DNN_USE_IMAGE_API
+DEFINES += DNN_USE_IMAGE_API \
+           CNN_USE_SSE \
+           CNN_USE_AVX \
+           NDEBUG
 #           HAVE_CXXABI_H
 
-QMAKE_CXXFLAGS += -std=c++14 -m64 -msse -msse2 -pthread -g -isystem
+QMAKE_CXXFLAGS += -std=gnu++14 -m64 -mavx -msse4 -pthread -g -isystem 03
+QMAKE_CXXFLAGS_RELEASE -= -O2
+QMAKE_CXXFLAGS_RELEASE += -O3
+
+QMAKE_LFLAGS_RELEASE -= -O1
 
 MOC_DIR = Build/.moc
 RCC_DIR = Build/.rcc
--- ENN/Sources/enn_chapar.cpp
+++ ENN/Sources/enn_chapar.cpp
@@ -28,24 +28,12 @@ void EnnChapar::epochLog()
 void EnnChapar::createEnn(QString word)
 {
     // add layers
-    // add layers
-    core::backend_t backend_type = core::default_engine();
-    net << conv(32, 32, 5, 1, 6,   // C1, 1@32x32-in, 6@28x28-out
-                 padding::valid, true, 1, 1, 1, 1, backend_type)
-         << activation::tanh()
-         << ave_pool(28, 28, 6, 2)   // S2, 6@28x28-in, 6@14x14-out
-         << activation::tanh()
-         << conv(14, 14, 5, 6, 16,   // C3, 6@14x14-in, 16@10x10-out
-                 core::connection_table(tbl, 6, 16),
-                 padding::valid, true, 1, 1, 1, 1, backend_type)
-         << activation::tanh()
-         << ave_pool(10, 10, 16, 2)  // S4, 16@10x10-in, 16@5x5-out
-         << activation::tanh()
-         << conv(5, 5, 5, 16, 120,   // C5, 16@5x5-in, 120@1x1-out
-                 padding::valid, true, 1, 1, 1, 1, backend_type)
-         << activation::tanh()
-         << fc(120, 10, true, backend_type)  // F6, 120-in, 10-out
-         << activation::tanh();
+    net << conv(32, 32, 5, 1, 6)   << activation::tanh()
+        << ave_pool(28, 28, 6, 2)  << activation::tanh()
+        << conv(14, 14, 5, 6, 16)  << activation::tanh()
+        << ave_pool(10, 10, 16, 2) << activation::tanh()
+        << conv(5, 5, 5, 16, 120)  << activation::tanh()
+        << fc(120, 10)             << activation::tanh();
 
     assert(net.in_data_size() == 32 * 32);
     assert(net.out_data_size() == 10);
@@ -58,9 +46,6 @@ void EnnChapar::createEnn(QString word)
 
     parse_mnist_labels("t10k-labels.idx1-ubyte", &test_labels);
     parse_mnist_images("t10k-images.idx3-ubyte", &test_images, -1.0, 1.0, 2, 2);
-
-//    assert(net.in_data_size() == 40 * 40);
-//    assert(net.out_data_size() == 2);
 
 //    parseImagesT(ENN_TRAIN_DIR, word);
 //    parseImagesF(ENN_TRAIN_DIR, word);
