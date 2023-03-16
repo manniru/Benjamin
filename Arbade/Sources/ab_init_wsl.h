2116ada8b40d8b7ddebb83ef86a0a9a1831e6b45
bijanbina <bijanbina@gmail.com>
2023-03-16 15:14:46 +0330

    ArBade: Implement Uncompressing WSL File System [Ehsan]

--- Arbade/Sources/ab_init_wsl.h
+++ Arbade/Sources/ab_init_wsl.h
@@ -3,6 +3,12 @@
 
 #include <QObject>
 #include <QDir>
+#include <QtNetwork>
+#include <windows.h>
+#include "backend.h"
+#include "config.h"
+
+#define AB_IMAGE_URL "https://github.com/bijanbina/KalB/releases/download/WSL/KalB.rar"
 
 class AbInitWSL : public QObject
 {
@@ -12,6 +18,27 @@ public:
     ~AbInitWSL();
 
     QString getWslPath();
+
+public slots:
+    void createWSL(QString drive);
+
+private slots:
+
+private:
+    void downloadImage(QString path);
+    void uncompImage(QString path); // uncompress image
+    int checkWSLInstalled();
+
+    QNetworkAccessManager net_man;
+    QNetworkReply* reply;
+    QFile rar_file;
 };
+
+QString getLinkPath(QString name);
+QString getLinkPathA(QString name);
+QString getLinkPathB(QString name);
+QString findAppPath(QString path, QString pattern,
+                    QString black_list);
+HRESULT resolveIt(LPCSTR lnk_path, char *target);
 
 #endif // AB_INIT_WSL_H
